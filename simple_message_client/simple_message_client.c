c46a14dc2a6a81b5bf6c3cbd4e540f0f6b74b93d
Thomas Halwax <thomas.halwax@syncpoint.io>
2015-11-26 14:42:21 +0100

    initial commit

---
+++ simple_message_client/simple_message_client.c
@@ -0,0 +1,44 @@
+//
+//
+//  simple_message_client
+//
+//  Created by Thomas {Halwax,Zeitinger} on 24.11.15.
+//  Copyright © 2015 Technikum Wien. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "simple_message_client_commandline_handling.h"
+
+void showUsage(FILE *stream, const char *cmnd, int exitcode);
+
+int main(int argc, const char * argv[]) {
+    /*
+     simple_message_client  -s server -p port -u user [-i image URL] -m mes-
+     sage [-v] [-h]
+     */
+    const char *server;
+    const char *port;
+    const char *user;
+    const char *image_url;
+    const char *message;
+    int verbose;
+    
+    
+    smc_parsecommandline(argc, argv, showUsage, &server, &port, &user, &message, &image_url, &verbose);
+    printf("All params parsed correctly\n");
+    return 0;
+}
+
+
+void showUsage(FILE *stream, const char *cmnd, int exitcode) {
+    fprintf(stream, "%s: %s\n\n", cmnd, "invalid params");
+    fprintf(stream, "%s: %s\n", cmnd, "-s server -p port -u user [-i image URL] -m message [-v] [-h]");
+    exit(exitcode);
+}
+
+
+
+
+
+
---
+++ simple_message_server/simple_message_server.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  simple_message_server
+//
+//  Created by Thomas {Halwax,Zeitinger} on 24.11.15.
+//  Copyright © 2015 Technikum Wien. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
