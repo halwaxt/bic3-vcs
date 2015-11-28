3692d43371453df46521dc8b8a10095eb9b6b9e0
Tom <ic14b033@technikum-wien.at>
2015-11-28 17:32:16 +0100

    first draft of client, next step: implement protocol

--- simple_message_client/simple_message_client.c
+++ simple_message_client/simple_message_client.c
@@ -10,7 +10,12 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <string.h>
 #include "simple_message_client_commandline_handling.h"
+
+#define BUF_SIZE 6000 /* Examples in protocol analysis were about 4500 bytes */
 
 void showUsage(FILE *stream, const char *cmnd, int exitcode);
 
@@ -26,39 +31,86 @@ int main(int argc, const char * argv[]) {
     const char *message;
     int verbose;
 
-    int socketfd = -1;
-	struct addrinfo clientsock;
-    memset(&clientsock, 0, sizeof(clientsock)); /* Ausnullen */
-	struct addrinfo 
-
-    
     smc_parsecommandline(argc, argv, showUsage, &server, &port, &user, &message, &image_url, &verbose);
     printf("All params parsed correctly\n");
-    return 0;
 
+    /* ToDos:
+     * Create connection to server
+     * Send message
+     * wait for reply
+     *
+     * see man getaddrinfo
+     */
 
-// todos:
-// connection
-// message send
-// waiting for response
+    struct addrinfo hints;
+    struct addrinfo *result, *rp;
+    int sfd, s, j;
+    size_t len;
+    ssize_t nread;
+    char buf[BUF_SIZE];
 
-	if ( (socketfd = socket(PF_UNSPEC, SOCK_STREAM, TCP)) == -1 ) {
-		close(socketfd);
-		fprintf(stderr, "Can not create socket\n");
-		exit(EXIT_FAILURE);
-	}
+    /* Obtain address(es) matching host/port */
 
-	//Howto check if ipv4 or ipv6?
-    //socketfd = socket(PF_INET, SOCK_STREAM, TCP);
-    //socketfd = socket(PF_INET6, SOCK_STREAM, TCP);
-    
-	/* try ipv6 first, if fail: connect ipv4 */
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
+    hints.ai_socktype = SOCK_STREAM; /* Stream socket */
+    hints.ai_flags = 0;
+    hints.ai_protocol = 0;          /* Any protocol */
 
-	if ( (connect(socketfd, (struct sockaddr*)&sa, len_sa)) == -1) {
-			// print ERROR
-			// errno?
-	}
+    s = getaddrinfo(server, port, &hints, &result);
+    if (s != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
+        exit(EXIT_FAILURE);
+    }
 
+    /* getaddrinfo() returns a list of address structures.
+       Try each address until we successfully connect(2).
+       If socket(2) (or connect(2)) fails, we (close the socket
+       and) try the next address. */
+
+    for (rp = result; rp != NULL; rp = rp->ai_next) {
+        sfd = socket(rp->ai_family, rp->ai_socktype,
+                     rp->ai_protocol);
+        if (sfd == -1)
+            continue;
+
+        if (connect(sfd, rp->ai_addr, rp->ai_addrlen) != -1)
+            break;                  /* Success */
+
+        close(sfd);
+    }
+
+    if (rp == NULL) {               /* No address succeeded */
+        fprintf(stderr, "Could not connect\n");
+        exit(EXIT_FAILURE);
+    }
+
+    freeaddrinfo(result);           /* No longer needed */
+
+    /* Send remaining command-line arguments as separate
+       datagrams, and read responses from server */
+
+    len = strlen(message) + 1; /* +1 for terminating null byte */
+
+    if (len > BUF_SIZE) {
+        fprintf(stderr, "Message to long");
+    }
+
+    /* TODO: write protocol stuff */
+    if (write(sfd, &message, len) != len) {
+        fprintf(stderr, "partial/failed write\n");
+        exit(EXIT_FAILURE);
+    }
+
+    nread = read(sfd, buf, BUF_SIZE);
+    if (nread == -1) {
+        perror("read");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Received %zd bytes: %s\n", nread, buf);
+
+    exit(EXIT_SUCCESS);
 }
 
 
