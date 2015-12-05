ea358ab7f53dc4abb985fefde080e353ce494101
Thomas Halwax <thomas.halwax@mindlab.at>
2015-12-05 19:22:22 +0100

    moved connect to server into separate function

--- simple_message_client/simple_message_client.c
+++ simple_message_client/simple_message_client.c
@@ -23,7 +23,7 @@
 
 void showUsage(FILE *stream, const char *cmnd, int exitcode);
 
-
+int connetToServer(const char *server, const char *port, int *socketDescriptor);
 int sendData(FILE *target, const char *key, const char *payload);
 int checkServerResponseStatus(FILE *source, int *status);
 int transferFile(FILE *source);
@@ -33,10 +33,7 @@ int getOutputFileName(FILE *source, char **value);
 static const char *programName;
 
 int main(int argc, const char * argv[]) {
-    /*
-     simple_message_client  -s server -p port -u user [-i image URL] -m mes-
-     sage [-v] [-h]
-     */
+
     const char *server;
     const char *port;
     const char *user;
@@ -48,54 +45,16 @@ int main(int argc, const char * argv[]) {
     
     smc_parsecommandline(argc, argv, showUsage, &server, &port, &user, &message, &image_url, &verbose);
 
-    struct addrinfo hints;
-    struct addrinfo *result, *rp;
-    int sfd = -1;
-    int s = -1;
-
-    /* Obtain address(es) matching host/port */
-
-    memset(&hints, 0, sizeof(struct addrinfo));
-    hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
-    hints.ai_socktype = SOCK_STREAM; /* Stream socket */
-    hints.ai_flags = 0;
-    hints.ai_protocol = 0;          /* Any protocol */
-
-    s = getaddrinfo(server, port, &hints, &result);
-    if (s != 0) {
-        fprintf(stderr, "%s: getaddrinfo: %s\n", programName, gai_strerror(s));
-        exit(EXIT_FAILURE);
+    int sfd = 0;
+    if (connetToServer(server, port, &sfd) != SUCCESS) {
+        exit(errno);
     }
-
-    /* getaddrinfo() returns a list of address structures.
-       Try each address until we successfully connect(2).
-       If socket(2) (or connect(2)) fails, we (close the socket
-       and) try the next address. */
-
-    for (rp = result; rp != NULL; rp = rp->ai_next) {
-        sfd = socket(rp->ai_family, rp->ai_socktype,
-                     rp->ai_protocol);
-        if (sfd == -1)
-            continue;
-
-        if (connect(sfd, rp->ai_addr, rp->ai_addrlen) != -1)
-            break;                  /* Success */
-
-        close(sfd);
-    }
-
-    if (rp == NULL) {               /* No address succeeded */
-        fprintf(stderr, "%s: Could not connect\n", programName);
-        exit(EXIT_FAILURE);
-    }
-
-    freeaddrinfo(result);           /* No longer needed */
-
+    
     errno = SUCCESS;
     FILE *toServer = fdopen(sfd, "w");
     if (toServer == NULL) {
         perror(programName);
-        shutdown(sfd, SHUT_RDWR);
+        close(sfd);
         exit(errno);
     }
     
@@ -127,7 +86,6 @@ int main(int argc, const char * argv[]) {
     int backupOfSfd = dup(sfd);
     
     shutdown(sfd, SHUT_WR);
-    /* destroys also sfd */
     fclose(toServer);
     
     FILE *fromServer = fdopen(backupOfSfd, "r");
@@ -161,6 +119,55 @@ int main(int argc, const char * argv[]) {
     fclose(fromServer);
     close(backupOfSfd);
     exit(EXIT_SUCCESS);
+}
+
+
+int connetToServer(const char *server, const char *port, int *socketDescriptor) {
+    struct addrinfo hints;
+    struct addrinfo *result, *rp;
+    int sfd = -1;
+    
+    /* Obtain address(es) matching host/port */
+    
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
+    hints.ai_socktype = SOCK_STREAM; /* Stream socket */
+    hints.ai_flags = 0;
+    hints.ai_protocol = 0;          /* Any protocol */
+    
+
+    if (getaddrinfo(server, port, &hints, &result) != SUCCESS) {
+        fprintf(stderr, "%s: getaddrinfo: %s\n", programName, gai_strerror(errno));
+        return ERROR;
+    }
+    
+    /* getaddrinfo() returns a list of address structures.
+     Try each address until we successfully connect(2).
+     If socket(2) (or connect(2)) fails, we (close the socket
+     and) try the next address. */
+    
+    for (rp = result; rp != NULL; rp = rp->ai_next) {
+        sfd = socket(rp->ai_family, rp->ai_socktype,
+                     rp->ai_protocol);
+        if (sfd == -1)
+            continue;
+        
+        if (connect(sfd, rp->ai_addr, rp->ai_addrlen) != -1)
+            break;                  /* Success */
+        
+        close(sfd);
+    }
+
+    freeaddrinfo(result);           /* No longer needed */
+    
+    if (rp == NULL) {               /* No address succeeded */
+        fprintf(stderr, "%s: Could not connect\n", programName);
+        return ERROR;
+    }
+    
+    socketDescriptor = &sfd;
+    return SUCCESS;
+
 }
 
 
