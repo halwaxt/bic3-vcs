7e7b74bb3863206b2d8daeb333aa68f67684d79c
thomas.halwax@mindlab.at <thomas.halwax@mindlab.at>
2015-12-08 19:48:21 +0100

    removed a lot

--- simple_message_server/simple_message_server.c
+++ simple_message_server/simple_message_server.c
@@ -20,7 +20,7 @@
 #include <getopt.h>
 #include <errno.h>
 #include <limits.h>
-#include "simple_message_client_commandline_handling.h"
+#include <signal.h>
 
 #define ERROR -1
 #define SUCCESS 0
@@ -48,14 +48,7 @@ int main(int argc, const char * argv[]) {
 	int opt = 0;
 	int port = -1;
 	const char *portstring;
-
-    int sfd = 0;
-	int cfd = 0;
-    pid_t childpid;
-    socklen_t clientLen;
-    struct sockaddr_in peerAddress;
-	struct sockaddr_in myAddress;
-    struct sigaction sa; /* for wait-child-handler */
+  
 
 	INFO("main()", "argc = %d", argc);
 	if(argc < 2) {
@@ -70,6 +63,7 @@ int main(int argc, const char * argv[]) {
 	};
 	int long_index =0;
 	while ((opt = getopt_long(argc, (char ** const) argv, "p:v", long_options, &long_index)) != -1) {
+        INFO("found param %s", opt);
 		switch(opt) {
 			case 'p':
 				portstring = optarg;
@@ -84,99 +78,7 @@ int main(int argc, const char * argv[]) {
 		}
 	}
 
-	INFO("main()", "start creating socket %s", "");
-    sfd = socket(AF_INET, SOCK_STREAM, 0);
-    if(sfd < 0)
-    {
-        perror("socket() error..");
-		close(sfd);
-        exit(EXIT_FAILURE);
-    }
-	INFO("main()", "socket created %s", "");
-
-	INFO("main()", "preparing connection infos %s", "");
-    memset(&myAddress, 0, sizeof(struct sockaddr_in));
-    myAddress.sin_family = AF_INET;
-    myAddress.sin_port = htons(port); /* bind server to port */
-    myAddress.sin_addr.s_addr = htonl(INADDR_ANY); /* bind server to all interfaces */
-
-	INFO("main()", "start binding socket to %d:%u", myAddress.sin_addr.s_addr, port);
-    if(bind(sfd, (struct sockaddr*) &myAddress, sizeof(struct sockaddr_in)) != SUCCESS)
-    {
-        perror("bind() failed..");
-        close(sfd);
-        exit(EXIT_FAILURE);
-    }
-	INFO("main()", "binding succeeded %s", "");
-
-	INFO("main()", "start listening with Queue-Size %d", LISTENQ);
-    if(listen(sfd, LISTENQ) < 0)
-    {
-        perror("listen() error..");
-        close(sfd);
-        exit(EXIT_FAILURE);
-    }
-	INFO("main()", "listening %s", "");
-
-
-	INFO("main()", "preparing sigaction() %s", "");
-    memset(&sa, 0, sizeof(sa));
-    sa.sa_handler = sigchld_handler;
-    sigaction(SIGCHLD, &sa, NULL);
-
-
-    /* endless listening-loop 
-       this loop waits for client-connections.
-       if a client is connectd it forks into another
-       subprocess.
-     */
-	INFO("main()", "starting loop for client acception %s", "");
-    while(1)
-    {   
-            clientLen = sizeof(peerAddress);
-			INFO("main()", "clientLen = %d", clientLen);
-
-			INFO("main()", "accepting connection from client %s", "");
-            cfd = accept(sfd, (struct sockaddr *) &peerAddress, &clientLen);
-            if(cfd < 0)
-            {
-                /*
-                   withouth this we would recieve:
-                "accept() error..: Interrupted system call" after
-                each client disconnect. this happens because SIGCHLD
-                blocks our parent-accept(). 
-                 */
-                if (errno == EINTR)
-                    continue;
-
-                    perror("accept() error..");
-                    close(sfd);
-                    exit(EXIT_FAILURE);
-            }
-
-			INFO("main()", "create subprocess: fork() %s", "");
-            childpid = fork();
-            if(childpid < 0)
-            {
-                perror("fork() failed");
-				close(sfd);
-                exit(EXIT_FAILURE);
-            }
-
-			INFO("main()", "start cild-subprocess %s", "");
-            if( childpid  == 0 )
-            {
-                close(sfd);
-                handle_client(cfd);
-            }
-			INFO("main()", "client has PID=%i", childpid);
-
-			INFO("main()", "continue server %s", "");
-
-			INFO("main()", "closing client filedescriptor %s", "");
-            close(cfd);
-    }
-
+    
     return EXIT_SUCCESS;
 }
 
@@ -186,31 +88,7 @@ void printUsage(const char *programName) {
 	exit(EXIT_FAILURE);	
 }
 
-int convertString2Int(const char *string) {
-	INFO("convertString2Int()", "convert port string (%s) to interger", string);
 
-	char *endptr;
-	long val;
-
-	errno = 0;
-
-	val = strtol(string, &endptr, 10);
-
-	if ((errno == ERANGE && (val == LONG_MAX || val == LONG_MIN))
-		|| (errno != 0 && val == 0)) {
-			perror("strtol");
-			exit(EXIT_FAILURE);
-	}
-
-	if (endptr == string) {
-		fprintf(stderr, "No digits were found\n");
-		exit(EXIT_FAILURE);
-	}
-
-	INFO("convertString2Int()", "converted string %s to integer %ld", string, val);
-	return val;
-
-}
 
 void sigchld_handler(int signo) {
 	signo = signo;
@@ -247,3 +125,4 @@ void handle_client(int cfd) {
 	INFO("handle_client()", "starting simple_message_server_logic %s", "");
 	execl("/usr/local/bin/simple_message_server_logic", "simple_message_server_logic", (char *) NULL);
 }
+
